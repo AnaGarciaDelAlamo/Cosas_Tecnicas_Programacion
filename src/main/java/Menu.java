5e385cba957cf5bc07b01a8b1af8dd7bd7e1c384
ana <andelamo@myuax.com>
2023-03-16 19:51:51 +0100

    Apartado a terminado

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -3,7 +3,10 @@ public class Menu {
     Scanner sc = new Scanner(System.in);
     public void menu(){
         System.out.println("MENÚ PRINCIPAL");
+        System.out.println("0.- Salir");
         System.out.println("1. Función recursiva");
+        System.out.println("Otra forma función recursiva");
+        
     }
 
 }
