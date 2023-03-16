cd2b1d99854768c7a7026991a72fdb8310e335b1
ana <andelamo@myuax.com>
2023-03-16 19:29:10 +0100

    Funcion recursiva y nueva clase

--- src/main/java/Funciones.java
+++ src/main/java/Funciones.java
@@ -1,2 +1,15 @@
 public class Funciones {
+    public double funcionRecursiva(double x, double y){
+        int i;
+        int contador = 0;
+        double resultado = 0;
+        do{
+            for(i = 0; i < y; i++){
+                 resultado = Math.pow(x, y);
+                contador++;
+            }
+
+        }while(contador != y);
+        return resultado;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Funciones funcion = new Funciones();
+        System.out.println(funcion.funcionRecursiva(5, 3));
 
     }
 }
